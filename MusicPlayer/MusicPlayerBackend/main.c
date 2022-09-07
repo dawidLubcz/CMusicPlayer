ca363bf799e79badb265d46bcaa2fd503bda01bb
dawidLubcz <dawidLubcz@email.com>
2022-09-07 20:17:20 +0200

    main.c cleanup.

--- MusicPlayer/MusicPlayerBackend/main.c
+++ MusicPlayer/MusicPlayerBackend/main.c
@@ -48,122 +48,160 @@ int main()
     int iOption = 0;
     do
     {
-        printf("Choose an option:\n 0->PLAY\n1->STOP\n2->PAUSE\n3->NEXT\n4->PREV\n5->SET_TRACK\n6->UNLOAD\n-1->QUIT\n7->Vol Up\n8->Vol Down\n9->SetVol\n10->List files\n11->List MP3\n12->set track with index\n13->set time position\n14->repeat ALL\n15->repeat ONE\n16->list folder\n17->list folder R\n");
+        printf("\
+\n+========== Command line options ==========+\n \
+        Choose an option:\n \
+        0->QUIT\n \
+        1->PLAY\n \
+        2->STOP\n \
+        3->PAUSE\n \
+        4->NEXT\n \
+        5->PREV\n \
+        6->SET_TRACK (by name)\n \
+        7->UNLOAD\n \
+        8->Vol Up\n \
+        9->Vol Down\n \
+        10->SetVol (0 - 128)\n \
+        11->List files\n \
+        12->List MP3\n \
+        13->Set track with index\n \
+        14->Set time position\n \
+        15->Repeat ALL\n \
+        16->Repeat ONE\n \
+        17->Create playlist from directory (path)\n \
+        18->Create playlist from directory - recursive (root path)\n\
++==========================================+\n");
         scanf("%d", &iOption);
 
         switch(iOption)
         {
-        case 0:
-        {
-            pl_core_play();
-        }break;
+            case 1:
+            {
+                pl_core_play();
+            }break;
 
-        case 1:
-        {
-            pl_core_stop();
-        }break;
+            case 2:
+            {
+                pl_core_stop();
+            }break;
 
-        case 2:
-        {
-            pl_core_pause();
-        }break;
+            case 3:
+            {
+                pl_core_pause();
+            }break;
 
-        case 3:
-        {
-            pl_core_next();
-        }break;
+            case 4:
+            {
+                pl_core_next();
+            }break;
 
-        case 4:
-        {
-            pl_core_prev();
-        }break;
+            case 5:
+            {
+                pl_core_prev();
+            }break;
 
-        case 5:
-        {
-            pl_core_setTrack("max.mp3");
-        }break;
+            case 6:
+            {
+                const int MAX_LENGTH = 256;
+                char trackName[MAX_LENGTH];
+                memset(trackName, '\0', sizeof(char) * MAX_LENGTH);
+                printf("Get track name:\n");
+                scanf("%255s", trackName);
+                pl_core_setTrack(trackName);
+            }break;
 
-        case 6:
-        {
-            pl_core_unload();
-        }break;
+            case 7:
+            {
+                pl_core_unload();
+            }break;
 
-        case 7:
-        {
-            pl_core_volUp();
-        }break;
+            case 8:
+            {
+                pl_core_volUp();
+            }break;
 
-        case 8:
-        {
-            pl_core_volDown();
-        }break;
+            case 9:
+            {
+                pl_core_volDown();
+            }break;
 
-        case 9:
-        {
-            int vol = 0;
-            printf("Get Volume, 0-128:\n");
-            scanf("%d",&vol);
-            pl_core_setVol(vol);
-        }break;
+            case 10:
+            {
+                int vol = 0;
+                printf("Get Volume, 0-128:\n");
+                scanf("%d",&vol);
+                pl_core_setVol(vol);
+            }break;
 
-        case 10:
-        {
-            pl_core_createPlayListInCurrDir();
-        }break;
+            case 11:
+            {
+                pl_core_createPlayListInCurrDir();
+            }break;
 
-        case 11:
-        {
-            pl_core_createMP3PlaylistInCurrDir();
-        }break;
+            case 12:
+            {
+                pl_core_createMP3PlaylistInCurrDir();
+            }break;
 
-        case 12:
-        {
-            uint64_t vol = 0;
-            printf("Get Track index, 0-?:\n");
-            scanf("%lu",&vol);
+            case 13:
+            {
+                uint64_t vol = 0;
+                printf("Get Track index, 0-?:\n");
+                scanf("%lu",&vol);
 
-            PRINT_INF("MAIN, index: %lu",vol);
+                PRINT_INF("MAIN, index: %lu",vol);
 
-            pl_core_setTrackWithIndex(vol);
-        }break;
+                pl_core_setTrackWithIndex(vol);
+            }break;
 
-        case 13:
-        {
-            uint32_t vol = 0;
-            printf("Set track time, 0-?:\n");
-            scanf("%u",&vol);
+            case 14:
+            {
+                uint32_t vol = 0;
+                printf("Set track time, 0-?:\n");
+                scanf("%u",&vol);
 
-            pl_core_setTimePos(vol);
-        }break;
+                pl_core_setTimePos(vol);
+            }break;
 
-        case 14:
-        {
-            pl_core_setRepeat(E_REPEAT_ALL);
-        }break;
+            case 15:
+            {
+                pl_core_setRepeat(E_REPEAT_ALL);
+            }break;
 
-        case 15:
-        {
-            pl_core_setRepeat(E_REPEAT_ONE);
-        }break;
+            case 16:
+            {
+                pl_core_setRepeat(E_REPEAT_ONE);
+            }break;
 
-        case 16:
-        {
-            pl_core_createPlaylistFromDir("/home/dawid/Documents/MusicPlayer/CMusicPlayer/build-MusicPlayer-Desktop-Debug/MusicPlayerBackend/USB");
-        }break;
+            case 17:
+            {
+                const int MAX_LENGTH = 256;
+                char path[MAX_LENGTH];
+                memset(path, '\0', sizeof(char) * MAX_LENGTH);
+                printf("Get path:\n");
+                scanf("%255s", path);
+                pl_core_createPlaylistFromDir(path);
+            }break;
 
-        case 17:
-        {
-            pl_core_createPlaylistFromDir_r("/home/dawid/Documents/MusicPlayer/CMusicPlayer/build-MusicPlayer-Desktop-Debug/MusicPlayerBackend/USB");
-        }break;
+            case 18:
+            {
+                const int MAX_LENGTH = 256;
+                char path[MAX_LENGTH];
+                memset(path, '\0', sizeof(char) * MAX_LENGTH);
+                printf("Get path:\n");
+                scanf("%255s", path);
+                pl_core_createPlaylistFromDir_r(path);
+            }break;
 
-        case -1:
-        {
-            pl_core_unload();
-        }break;
+            default:
+            case -1:
+            {
+                pl_core_unload();
+            }break;
 
         }
 
-    }while(iOption != (-1));
+    }while(iOption != (-1) && iOption > 0 && iOption <= 18);
 
     pl_core_deinitialize();
 
