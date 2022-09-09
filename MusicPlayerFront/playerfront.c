70ebda49d0814c3c667d750a7e6c41465922e19b
Dawid Zielinski <dawid.zielinski@notexists.com>
2022-09-09 13:34:00 +0200

    Cleanup in player front.

---
+++ MusicPlayerFront/playerfront.c
@@ -0,0 +1,47 @@
+#include "playerfront.h"
+
+
+static const char* g_pcMsqQueueFile = "msgQueueFile";
+static char g_cProjectID = 'D' | 'L';
+static int32_t  g_i32MsgQueueID = 0;
+
+
+int frontInitialize()
+{
+    key_t u32MsgQueueKey = -1;
+    int i32Result = -1;
+
+    u32MsgQueueKey = ftok(g_pcMsqQueueFile, g_cProjectID);
+    if(-1 < u32MsgQueueKey)
+    {
+        if(-1 == (g_i32MsgQueueID = msgget(u32MsgQueueKey, 0644 | IPC_CREAT)))
+        {
+            perror("msgget");
+        }
+        else
+        {
+            i32Result = 0;
+        }
+    }
+    else
+    {
+        perror("ftok");
+    }
+    return i32Result;
+}
+
+int sendMessage(sMessage_t* a_sParam)
+{
+    int _fRetval = 0;
+    if(-1 != g_i32MsgQueueID && 0 != a_sParam)
+    {
+        a_sParam->mtype = 1;
+        if(-1 == msgsnd(g_i32MsgQueueID, a_sParam, sizeof(sMessage_t),0))
+        {
+            perror("msgsnd");
+        }
+        else
+            _fRetval = 1;
+    }
+    return _fRetval;
+}
